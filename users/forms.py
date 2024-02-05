b625d36ba3d4cae22b2b24727b53e428b797fb51
[joel] <[jbur645@wgu.edu]>
2024-02-05 11:56:51 -0500

    custom user model workflow and fields updated

--- users/forms.py
+++ users/forms.py
@@ -4,13 +4,15 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
 
-class CustomerUserCreationForm(UserCreationForm):
+class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm):
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ('age',)
+        fields = ('username', 'email', 'age')
 
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
-        fields = UserChangeForm.Meta.fields
+        fields = ('username', 'email', 'age')
+
+
