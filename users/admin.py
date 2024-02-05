ed0d661a153338027d34d3d48442e9e0a7a96066
[joel] <[jbur645@wgu.edu]>
2024-02-05 10:17:24 -0500

    custom user model completed

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import CustomerUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomerUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['email', 'username', 'age', 'is_staff', ]
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
